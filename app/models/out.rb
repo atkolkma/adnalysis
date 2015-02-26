219b901d9bece6e9aa193f5860780c9f985baaa6
atkolkma@gmail.com <atkolkma@gmail.com>
2015-02-26 18:29:36 -0500

    remove superclass mismatch error

--- app/models/out.rb
+++
@@ -1,9 +0,0 @@
-
-require 'smarter_csv'
-
-class Report
-	def new
-		file = Rails.root + 'data/Search-term-report2.csv'
-		@table = SmarterCSV.process(file, {file_encoding: 'iso-8859-1'})
-	end
-end
