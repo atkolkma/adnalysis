018a8b5120561a002c7cb016dd46c284cde69461
atkolkma@gmail.com <atkolkma@gmail.com>
2015-02-26 21:02:38 -0500

    destroy files on all branches of s3

--- app/models/source_file.rb
+++ app/models/source_file.rb
@@ -2,11 +2,11 @@ class SourceFile < ActiveRecord::Base
   belongs_to :data_set
 
   def s3_key
-  	self.remote_path.gsub("http://crunch-analytics-dev.s3.amazonaws.com/", '')
+  	self.remote_path.gsub(/^.*s3.amazonaws.com\//, '')
   end
 
   def s3_bucket
-  	"crunch-analytics-dev"
+  	ENV["S3_BUCKET"]
   end
 
   def name
